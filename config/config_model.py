9884a6860b7d0154226f18565ae9cad13c0b6be0
flinguen <florent@linguenheld.net>
2026-03-03 21:34:25 +0100

    Config - add field validators

--- config/config_model.py
+++ config/config_model.py
@@ -1,11 +1,11 @@
-from typing import Annotated, Tuple
-from pydantic import BaseModel, Field
+from typing import Annotated, Tuple, Any
+from pydantic import BaseModel, Field, field_validator, model_validator
 
 SIZE_MIN = 2
 SIZE_MAX = 500
 
 
-# manage 42 in the middle
+# TODO: manage 42 in the middle
 
 
 # ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
@@ -21,3 +21,37 @@ class ConfigModel(BaseModel):
         str, Field(min_length=3, max_length=30, alias="OUTPUT_FILE")
     ]
     perfect: Annotated[bool, Field(alias="PERFECT")]
+
+    # #########################################################################
+    # ########               ⡇⢸ ⢀⣀ ⡇ ⠄ ⢀⣸ ⢀⣀ ⣰⡀ ⢀⡀ ⡀⣀ ⢀⣀               ########
+    # ########               ⠸⠃ ⠣⠼ ⠣ ⠇ ⠣⠼ ⠣⠼ ⠘⠤ ⠣⠜ ⠏  ⠭⠕               ########
+
+    # #########################################################################
+    # ###################### ENTRY & EXIT VALUES HAVE TO BE HIGHER THAN 0 #####
+    @field_validator("entry", "exit")
+    @classmethod
+    def does_id_start_with_m(cls, value: Tuple[int, int]) -> Tuple[int, int]:
+        row, col = value
+        if row < 0 or col < 0:
+            raise ValueError("Coordinates have to be higher than 0")
+        return value
+
+    # #########################################################################
+    # ############################### ENTRY & EXIT HAVE TO BE IN THE MAZE #####
+    @model_validator(mode="after")
+    def are_in_the_maze(self) -> Any:
+
+        entry_row, entry_col = self.entry
+        exit_row, exit_col = self.exit
+
+        if entry_row > self.height:
+            raise ValueError(f"Entry Y can't be higher than {self.height}")
+        if exit_row > self.height:
+            raise ValueError(f"Exit Y can't be higher than {self.height}")
+
+        if entry_col > self.width:
+            raise ValueError(f"Entry X can't be higher than {self.width}")
+        if exit_col > self.width:
+            raise ValueError(f"Exit X can't be higher than {self.width}")
+
+        return self
--- config/config_parser.py
+++ config/config_parser.py
@@ -11,33 +11,33 @@ class ConfigParser:
         self.__file_name = file_name
 
     @staticmethod
-    def parse_line(line: str, dictionary: dict[str, str | Tuple[str, str]]):
+    def __parse_line(line: str, dictionary: dict[str, str | Tuple[str, str]]):
         """
-        Split the line in two with =
-        Split again with , if necessary
-        Save the pair in the given dictionary
+        - Split the line in two with =
+        - Split again with , if necessary
+        - Save the pair in the given dictionary
         """
         if not line.startswith("#"):
             key, value = line.split("=")
 
             if "," in value:
-                left, right = value.strip().split(",")
-                dictionary[key] = (left, right)
+                col, row = value.strip().split(",")
+                dictionary[key] = (row, col)
             else:
                 dictionary[key] = value.strip()
 
     def parse_file(self) -> dict[str, int | bool | str | Tuple[int, int]]:
         """
-        Open the config file
-        Parse each line to feed a model
-        Return a dictionary with all config options
+        - Open the config file
+        - Parse each line to feed a model
+        - Return a dictionary with all config options
 
         Raise Value error if forbidden, missing or invalid data
         """
         parsed_values = {}
         with open(self.__file_name, "r") as f:
             for line in f.readlines():
-                ConfigParser.parse_line(line, parsed_values)
+                ConfigParser.__parse_line(line, parsed_values)
 
         parser = ConfigModel.model_validate(parsed_values)
         return ConfigModel.model_dump(parser)
