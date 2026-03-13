ce7f7ff30095e82cca93047abe1c9a131d0342b9
cali-pso <raphael.poggi@laposte.net>
2026-03-13 16:42:10 +0100

    Generator: split animate() in smaller functions - gave back instance attributes + fixed random seeding: default seed now handled by Maze

--- maze_generator/maze_generator.py
+++ maze_generator/maze_generator.py
@@ -6,8 +6,6 @@ from maze_generator.path_finder import PathFinder
 
 from typing import Optional, Set
 import random
-from datetime import datetime
-import time
 
 # TODO: Build installable via pip package (uv build, .whl)
 
@@ -24,53 +22,86 @@ class MazeGenerator:
         if config is not None:
             self.config = config
         else:
-            # TODO: Implement defaults straight in Config
-            self.config = Config.model_validate(
-                {
-                    "WIDTH": 10,
-                    "HEIGHT": 10,
-                    "ENTRY": (0, 0),
-                    "EXIT": (9, 9),
-                    "OUTPUT_FILE": "dummy.txt",
-                    "PERFECT": True,
-                }
-            )
+            self.config = Config()
 
-    @staticmethod
-    def destroy_walls(maze: Maze, full_path: Set[tuple[int, int]]):
+    def destroy_walls(self):
         """
         - Receives a Maze object and a set of all cells coordinates
         - Iterates through all visited cells
         - Breaks random wall if not near a 0 cell/nor creating a 0 cell
         - Yield Maze at each wall broken
         """
-        for cur_r, cur_c in full_path:
+        for cur_r, cur_c in self.full_path:
             if not (
-                0 > cur_r > maze.nb_row - 2
-                and 0 > cur_c > maze.nb_col - 2
-                and maze.values[cur_r][cur_c] != 0
+                0 > cur_r > self.maze.nb_row - 2
+                and 0 > cur_c > self.maze.nb_col - 2
+                and self.maze.values[cur_r][cur_c] != 0
             ):
                 pass
             try:
-                if random.randint(0, 1) != 1:
+                if self.rand.randint(0, 1) != 1:
                     raise ValueError
                 for y in range(-1, 1):
                     for x in range(-1, 1):
                         if (
-                            0 >= cur_r - y >= maze.nb_row - 1
-                            and 0 >= cur_c - x >= maze.nb_col - 1
-                            and (cur_r - y, cur_c - x) not in maze.cells_42
-                            and maze.values[cur_r - y][cur_c - x] == 0b0000
+                            0 >= cur_r - y >= self.maze.nb_row - 1
+                            and 0 >= cur_c - x >= self.maze.nb_col - 1
+                            and (cur_r - y, cur_c - x)
+                            not in self.maze.cells_42
+                            and self.maze.values[cur_r - y][cur_c - x]
+                            == 0b0000
                         ):
                             raise ValueError
                 neighbours = {
-                    (cur_r - r, cur_c - c) for r, c in maze.WALL_MAP.keys()
+                    (cur_r - r, cur_c - c)
+                    for r, c in self.maze.WALL_MAP.keys()
                 }
-                ngbr = random.choice([i for i in neighbours if i in full_path])
-                maze.break_wall((cur_r, cur_c), ngbr, True)
-                yield maze
+                ngbr = self.rand.choice(
+                    [i for i in neighbours if i in self.full_path]
+                )
+                self.maze.break_wall((cur_r, cur_c), ngbr, True)
+                yield self.maze
             except ValueError:
                 pass
+
+    def reset_attributes(self) -> Maze:
+        self.rand = random.Random(self.config.get("seed"))
+
+        self.maze = Maze(
+            self.config.nb_row,
+            self.config.nb_col,
+            self.config.entry,
+            self.config.exit,
+            self.config.perfect,
+            self.config.seed,
+        )
+
+        self.full_path: Set[tuple[int, int]] = set()
+        self.not_visited = {
+            (r, c)
+            for r in range(self.maze.nb_row)
+            for c in range(self.maze.nb_col)
+            if (r, c) not in self.maze.cells_42
+        }
+
+        return self.maze
+
+    def find_first_path(self) -> Maze:
+        path = Walker(self.maze, False, self.maze.start, self.maze.end).walk(
+            self.rand
+        )
+        self.full_path.update(path)
+        self.not_visited.difference_update(path)
+
+    def find_next_path(self) -> Maze:
+        choice = self.rand.choice(sorted(list(self.not_visited)))
+        path = Walker(self.maze, False, choice, self.full_path).walk(self.rand)
+        self.full_path.update(path)
+        self.not_visited.difference_update(path)
+
+    def solve_maze(self) -> Maze:
+        self.maze.set_solution(PathFinder(self.maze, self.config).search())
+        return self.maze
 
     def generate(self) -> Maze | None:
         """
@@ -90,51 +121,19 @@ class MazeGenerator:
         - Find quickest path from entry to exit and write solution to maze
         - yield last maze
         """
-        if self.config.get("seed") is None:
-            random.seed(
-                f"{datetime.now().strftime('%Y%m%d%H%M%S')}{time.time()}"
-            )
-        else:
-            random.seed(self.config.get("seed"))
+        self.reset_attributes()
+        yield self.maze
 
-        maze = Maze(
-            self.config.nb_row,
-            self.config.nb_col,
-            self.config.entry,
-            self.config.exit,
-            self.config.perfect,
-        )
+        self.find_first_path()
+        yield self.maze
 
-        full_path: Set[tuple[int, int]] = set()
-        not_visited = {
-            (r, c)
-            for r in range(maze.nb_row)
-            for c in range(maze.nb_col)
-            if (r, c) not in maze.cells_42
-        }
+        while len(self.not_visited) != 0:
+            self.find_next_path()
+            yield self.maze
 
-        yield maze
+        if self.maze.perfect is False:
+            for maze in MazeGenerator.destroy_walls():
+                yield maze
 
-        path = Walker(maze, False, maze.start, maze.end).walk()
-        full_path.update(path)
-        not_visited.difference_update(path)
-
-        yield maze
-
-        while len(not_visited) != 0:
-            choice = random.choice(list(not_visited))
-            path = Walker(maze, False, choice, full_path).walk()
-            full_path.update(path)
-            not_visited.difference_update(path)
-            print(self.config.get("nb_col"))
-
-            yield maze
-
-        if maze.perfect is False:
-            for wmaze in MazeGenerator.destroy_walls(maze, full_path):
-                yield wmaze
-                maze = wmaze
-
-        maze.set_solution(PathFinder(maze, self.config).search())
-
-        yield maze
+        self.solve_maze()
+        yield self.maze
